32a50cc4f82628728172552dad6a8ce6eb59005a
JamesK-XIV <jameskidman14@gmail.com>
2023-10-05 20:48:31 +1100

    First version of script finished, next gonna work on player looking way he is moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //private Tween activeTween;
+    private Tween activeTween = null;
+    [SerializeField]
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activeTween != null)
+        {
+            if (Vector3.Distance(player.transform.position, activeTween.EndPos) > 0.1f)
+            {
+                float percentage = (Time.time - activeTween.StartTime) / Vector3.Distance(activeTween.StartPos, activeTween.EndPos);
+                player.transform.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, percentage);
+            }
+            if (Vector3.Distance(player.transform.position, activeTween.EndPos) < 0.1f)
+            {
+                player.transform.position = activeTween.EndPos;
+                activeTween = null;
+            }
+        }
+        AddTween();
+    }
+    public void AddTween()
+    {
+        if (Vector3.Distance(player.transform.position, new Vector3(6, -1, 0)) < 0.1f)
+        {
+            activeTween = new Tween(player.transform.position, new Vector3(6, -5, 0), Time.time);
+        }
+        else if (Vector3.Distance(player.transform.position, new Vector3(1, -1, 0)) < 0.1f)
+        {
+            activeTween = new Tween(player.transform.position, new Vector3(6, -1, 0), Time.time);
+        }
+        else if (Vector3.Distance(player.transform.position, new Vector3(6, -5, 0)) < 0.1f)
+        {
+            activeTween = new Tween(player.transform.position, new Vector3(1, -5, 0), Time.time);
+        }
+        else if (Vector3.Distance(player.transform.position, new Vector3(1, -5, 0)) < 0.1f)
+        {
+            activeTween = new Tween(player.transform.position, new Vector3(1, -1, 0), Time.time);
+        }
     }
 }
--- Assets/Scripts/Tween.cs
+++ Assets/Scripts/Tween.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Tween
 {
-    public Transform Target { get; private set; }
     public Vector3 StartPos { get; private set; }
     public Vector3 EndPos { get; private set; }
+    public float StartTime { get; private set; }
 
-    public Tween(Transform target, Vector3 startPos, Vector3 endPos)
+    public Tween(Vector3 startPos, Vector3 endPos, float time)
     {
-        Target = target;
         StartPos = startPos;
         EndPos = endPos;
+        StartTime = time;
     }
 
 }
