fd1c55f5c8fdfba263a590f1343b3374a247f522
JamesK-XIV <jameskidman14@gmail.com>
2023-10-31 15:06:43 +1100

    Finished Section (Exit btn returns to home screen)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -20,10 +21,25 @@ public class UIManager : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         SceneManager.LoadSceneAsync(1);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
     public void LoadImprovementLevel()
     {
         DontDestroyOnLoad (gameObject);
         SceneManager.LoadSceneAsync(2);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    public void LoadMainMenu()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadSceneAsync(0);
+    }
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
+    {
+        if (scene.buildIndex == 1)
+        {
+            Button button = GameObject.FindGameObjectWithTag("ExitButton").GetComponent<Button>();
+            button.onClick.AddListener(LoadMainMenu);
+        }
     }
 }
